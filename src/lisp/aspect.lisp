49af390238676807b5d14d69cf63627fd4f988b2
Mark <evenson@panix.com>
2012-08-02 11:31:10 +0200

    ???: speculative commit of an :around method for URI normalization.

---
+++ src/lisp/aspect.lisp
@@ -0,0 +1,9 @@
+(defgeneric org.example.normalizeURI (url))
+
+;;(defun trace 
+
+(defmethod :around org.example.normalizeURI (url url)
+  (push args)
+  (call-next-method)
+  (push result)
+  (return result))
